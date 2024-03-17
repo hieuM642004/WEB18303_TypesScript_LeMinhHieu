aa86dfc2cd72058f80705309098bf3799c4ac741
hieuM642004 <136356604+hieuM642004@users.noreply.github.com>
2024-03-17 15:18:07 +0700

    Assignment1_Submit

--- Assignment-TS/README.md
+++
@@ -1,4 +0,0 @@
-Start project
-1. npm install
-2. npm run dev
-3. http://localhost:5173/
---
+++ src/Lab3/app.ts
@@ -0,0 +1,48 @@
+//Bài 1
+//1.1
+// const sum=()=>{
+//     return 5+5
+// };
+// console.log(sum());
+//1.2
+// const sum=(a:number,b:number=3,...rest:number[])=>{
+// let total= a+b
+// if(rest.length>0){
+//     total+=rest.reduce((acc,cur)=>acc+cur)
+// }
+// return total
+// }
+// console.log(
+//     sum(5,5,...[1,2,3])
+// );
+//1.3
+// const hobbies=['Sports','Cooking'];
+// const activehobbies=['Hiking']
+// activehobbies.push(hobbies);
+// activehobbies.push(hobbies[0],hobbies[1]);
+// activehobbies.push(...hobbies)
+// console.log(activehobbies);
+//Bài 2
+//2.1
+let sum = (x: number, y?: number) => {
+    return x + <number>y;
+};
+let speech = (output: any): void => {
+    console.log('Result' + output);
+};
+speech(sum(5, 12));
+console.log(speech(sum(8, 5)));
+//2.2
+let something: void = undefined;
+let nothing: never = null;
+function throwError(erorrMsg: string): never {
+    throw new Error(erorrMsg);
+}
+//3.3
+function AddandHandle(x: number, y: number, cd: (num: number) => void) {
+    const result = x + y;
+    cd(result);
+}
+AddandHandle(10, 20, (result) => {
+    console.log(result);
+});
