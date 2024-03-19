b329260c15a9d588833b4b4c0e6fba2d7e589382
hieuM642004 <136356604+hieuM642004@users.noreply.github.com>
2024-03-19 21:06:43 +0700

    Lab3_Submit

---
+++ src/Lab3/app.js
@@ -0,0 +1,163 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+//Bài 1
+//1.1
+var sum1 = function () {
+    return 5 + 5;
+};
+console.log(sum1());
+function sumF() {
+    return 5 + 5;
+}
+;
+console.log(sum1());
+console.log(sumF());
+//1.2
+var sum2 = function (a, b) {
+    if (b === void 0) { b = 3; }
+    var rest = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        rest[_i - 2] = arguments[_i];
+    }
+    var total = a + b;
+    if (rest.length > 0) {
+        total += rest.reduce(function (acc, cur) { return acc + cur; });
+    }
+    return total;
+};
+console.log(sum2.apply(void 0, __spreadArray([5, 5], [1, 2, 3], false)));
+//1.3
+var hobbies = ['Sports', 'Cooking'];
+var activehobbies = ['Hiking'];
+activehobbies.push(hobbies); //Error Argument of type 'string[]' is not assignable to parameter of type 'string'.
+activehobbies.push(hobbies[0], hobbies[1]);
+activehobbies.push.apply(activehobbies, hobbies);
+console.log(activehobbies);
+//Bài 2
+//2.1
+var sum22 = function (x, y) {
+    return x + y;
+};
+var speech = function (output) {
+    console.log('Result' + output);
+};
+speech(sum22(5, 12));
+console.log(speech(sum22(8, 5)));
+//2.2
+var something = undefined;
+var nothing = null;
+function throwError(erorrMsg) {
+    throw new Error(erorrMsg);
+}
+//3.3
+function AddandHandle(x, y, cd) {
+    var result = x + y;
+    cd(result);
+}
+AddandHandle(10, 20, function (result) {
+    console.log(result);
+});
+//Bài cho thêm
+// Arrow function
+var multiplication = function (a, b) {
+    return a * b;
+};
+console.log(multiplication(5, 2));
+// Cú pháp: Arrow function có cú pháp ngắn gọn hơn so với hàm thông thường.
+// Binding của this: Trong arrow function, this được binding theo lexical scope,còn hàm thông thường this phụ thuộc vào cách hàm được gọi.
+// arguments: Arrow function không có đối tượng arguments, trong khi hàm thông thường có.
+// new: Arrow function không thể được sử dụng với new, nó không có [[Construct]] và sẽ throw một lỗi nếu cố gắng gọi nó với new.
+//Function return
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(5, 5));
+var reverseString = function (stri) {
+    return stri.split("").reverse().join(" ");
+};
+console.log(reverseString('Hello'));
+var addTwoNumber = function (a, b) {
+    return a + b;
+};
+//Function with parameters
+var sumArray = function (arr) {
+    return arr.reduce(function (acc, cur) { return acc + cur; });
+};
+console.log(sumArray([1, 2, 3, 4, 5]));
+var calculateSum = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (acc, cur) { return acc + cur; });
+};
+console.log(calculateSum(4, 6, 8));
+console.log(calculateSum(4, 6, 8, 9, 7, 3, 5));
+//Default parameter
+var squareNumber = function (a) {
+    if (a === void 0) { a = 7; }
+    return a * a;
+};
+console.log(squareNumber());
+var sumTwoNumber = function (a, b) {
+    if (b === void 0) { b = 2; }
+    return a + b;
+};
+console.log(sumTwoNumber(2));
+//Optional parameter
+var getValueOrDefault = function (value, defaultValue) {
+    if (defaultValue === void 0) { defaultValue = "Default"; }
+    return value !== undefined ? value : defaultValue;
+};
+console.log(getValueOrDefault('HII'));
+console.log(getValueOrDefault());
+//Spread operators
+var sumArr = function () {
+    var arr = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arr[_i] = arguments[_i];
+    }
+    return arr.reduce(function (acc, cur) { return acc + cur; });
+};
+var arrNumber = [1, 2, 3];
+console.log(sumArr.apply(void 0, arrNumber));
+//Rest parameter
+var sumRest = function () {
+    var arr = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arr[_i] = arguments[_i];
+    }
+    return arr.reduce(function (acc, cur) { return acc + cur; });
+};
+console.log(sumRest(1, 2, 3, 4, 5));
+console.log(sumRest(5, 10, 15, 20));
+//Function & void
+var voidFuncion1 = function () {
+    console.log('Thông báo');
+};
+console.log(voidFuncion1());
+var neverFuncion1 = function () {
+};
+console.log(neverFuncion1());
+//Never & void
+//Void
+// Kiểu void thường được sử dụng để chỉ ra rằng một hàm không trả về bất kỳ giá trị nào.
+// Hàm vẫn có thể thực hiện các công việc nội tại của nó, nhưng không trả về bất kỳ giá trị nào.
+// Một biến có kiểu void có thể chứa giá trị undefined hoặc null.
+//Never
+//Kiểu never là loại dữ liệu của các biến không bao giờ có giá trị.
+// Nó  sử dụng trong các trường hợp mà hàm không bao giờ kết thúc hoặc ném ra một exception.
+// Nếu một hàm có kiểu trả về never, nó không thể trả về bất kỳ giá trị nào, kể cả undefined hoặc null.
+var voidFuncion = function () { };
+var resultVoid = voidFuncion();
+var neverFuncion = function () {
+    throw new Error('Luôn ném ra một exception.');
+};
+var resultNever = neverFuncion();
--- src/Lab3/app.ts
+++ src/Lab3/app.ts
@@ -1,37 +1,42 @@
 //Bài 1
 //1.1
-// const sum=()=>{
-//     return 5+5
-// };
-// console.log(sum());
+const sum1=()=>{
+    return 5+5
+};
+console.log(sum1());
+function sumF(){
+    return 5+5
+};
+console.log(sum1());
+console.log(sumF());
 //1.2
-// const sum=(a:number,b:number=3,...rest:number[])=>{
-// let total= a+b
-// if(rest.length>0){
-//     total+=rest.reduce((acc,cur)=>acc+cur)
-// }
-// return total
-// }
-// console.log(
-//     sum(5,5,...[1,2,3])
-// );
+const sum2=(a:number,b:number=3,...rest:number[])=>{
+let total= a+b
+if(rest.length>0){
+    total+=rest.reduce((acc,cur)=>acc+cur)
+}
+return total
+}
+console.log(
+    sum2(5,5,...[1,2,3])
+);
 //1.3
-// const hobbies=['Sports','Cooking'];
-// const activehobbies=['Hiking']
-// activehobbies.push(hobbies);
-// activehobbies.push(hobbies[0],hobbies[1]);
-// activehobbies.push(...hobbies)
-// console.log(activehobbies);
+const hobbies:string[]=['Sports','Cooking'];
+const activehobbies:string[]=['Hiking']
+activehobbies.push(hobbies); //Error Argument of type 'string[]' is not assignable to parameter of type 'string'.
+activehobbies.push(hobbies[0],hobbies[1]);
+activehobbies.push(...hobbies)
+console.log(activehobbies);
 //Bài 2
 //2.1
-let sum = (x: number, y?: number) => {
+let sum22 = (x: number, y?: number) => {
     return x + <number>y;
 };
 let speech = (output: any): void => {
     console.log('Result' + output);
 };
-speech(sum(5, 12));
-console.log(speech(sum(8, 5)));
+speech(sum22(5, 12));
+console.log(speech(sum22(8, 5)));
 //2.2
 let something: void = undefined;
 let nothing: never = null;
@@ -46,3 +51,105 @@ function AddandHandle(x: number, y: number, cd: (num: number) => void) {
 AddandHandle(10, 20, (result) => {
     console.log(result);
 });
+
+//Bài cho thêm
+
+// Arrow function
+const multiplication=(a:number,b:number)=>{
+return a*b
+}
+console.log( multiplication(5,2));
+// Cú pháp: Arrow function có cú pháp ngắn gọn hơn so với hàm thông thường.
+// Binding của this: Trong arrow function, this được binding theo lexical scope,còn hàm thông thường this phụ thuộc vào cách hàm được gọi.
+// arguments: Arrow function không có đối tượng arguments, trong khi hàm thông thường có.
+// new: Arrow function không thể được sử dụng với new, nó không có [[Construct]] và sẽ throw một lỗi nếu cố gắng gọi nó với new.
+
+//Function return
+const sum=(a:number,b:number)=>{
+    return a+b
+    }
+    console.log(sum(5,5));
+
+const reverseString=(stri:string)=>{
+return stri.split("").reverse().join(" ")
+}
+console.log( reverseString('Hello'));
+
+//Function as types
+type TwoNumber=(a:number,b:number)=>number;
+const addTwoNumber:TwoNumber=(a,b)=>{
+return a+b;
+}
+
+//Function with parameters
+const sumArray=(arr:number[])=>{
+return arr.reduce((acc,cur)=>acc+cur)
+}
+console.log(sumArray([1,2,3,4,5]));
+
+const calculateSum = (...numbers: number[]): number => {
+return numbers.reduce((acc,cur)=>acc+cur)
+}
+console.log(calculateSum(4,6,8));
+console.log(calculateSum(4,6,8,9,7,3,5));
+
+//Default parameter
+const squareNumber=(a:number=7):number=>{
+return a*a
+}
+console.log(squareNumber());
+const sumTwoNumber=(a:number,b:number=2)=>{
+    return a+b;
+}
+console.log(sumTwoNumber(2));
+
+//Optional parameter
+const getValueOrDefault=(value?: any, defaultValue: any = "Default"):any=>{
+
+return value !== undefined ? value: defaultValue
+}
+console.log(getValueOrDefault('HII'));
+console.log(getValueOrDefault());
+
+//Spread operators
+const sumArr=(...arr:number[]):number=>{
+    return arr.reduce((acc,cur)=>acc+cur)
+}
+const arrNumber:number[]=[1,2,3]
+console.log(sumArr(...arrNumber));
+
+//Rest parameter
+const sumRest=(...arr:number[]):number=>{
+    return arr.reduce((acc,cur)=>acc+cur)
+}
+console.log(sumRest(1, 2, 3, 4, 5));
+console.log(sumRest(5, 10, 15, 20));
+
+//Function & void
+const voidFuncion1=():void=>{
+    console.log('Thông báo');
+
+}
+console.log(voidFuncion1());
+
+const neverFuncion1=():void=>{
+
+}
+console.log(neverFuncion1());
+
+//Never & void
+//Void
+// Kiểu void thường được sử dụng để chỉ ra rằng một hàm không trả về bất kỳ giá trị nào.
+// Hàm vẫn có thể thực hiện các công việc nội tại của nó, nhưng không trả về bất kỳ giá trị nào.
+// Một biến có kiểu void có thể chứa giá trị undefined hoặc null.
+//Never
+//Kiểu never là loại dữ liệu của các biến không bao giờ có giá trị.
+// Nó  sử dụng trong các trường hợp mà hàm không bao giờ kết thúc hoặc ném ra một exception.
+// Nếu một hàm có kiểu trả về never, nó không thể trả về bất kỳ giá trị nào, kể cả undefined hoặc null.
+
+const voidFuncion = (): void => {};
+const resultVoid: void = voidFuncion();
+const neverFuncion = (): never => {
+    throw new Error('Luôn ném ra một exception.');
+};
+const resultNever: never = neverFuncion();
